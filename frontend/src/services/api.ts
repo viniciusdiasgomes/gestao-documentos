e8f59f035ba9bc38ef28ac58ec31ba71a8319f4c
Vinícius Dias <viniciusdiasgomes@gmail.com>
2026-02-13 01:50:25 -0300

    configurando .env no frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,1 +1,9 @@
-export const API_URL = "http://localhost:3333";
+import axios from 'axios';
+
+// Exportando a URL para os arquivos que pedem 'API_URL'
+export const API_URL = import.meta.env.VITE_API_URL;
+
+// Criando e exportando a instância 'api'
+export const api = axios.create({
+  baseURL: API_URL,
+});
